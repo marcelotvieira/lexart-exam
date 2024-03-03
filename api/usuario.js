314fe78a01d2dbcb0a025746d454898a883fe909
marcelotvieira <marcelodefy15@gmail.com>
2024-03-03 13:22:02 -0300

    tests

--- api/usuario.js
+++ api/usuario.js
@@ -1,16 +1,18 @@
+import bodyParser from 'body-parser'
 import e from 'express'
 import rescue from 'express-rescue'
 import { UsuarioController } from '../modules/controllers/usuario'
 import { ApiError } from '../modules/error/ApiError'
+
 export const config = {
   api: {
     externalResolver: true,
-    bodyParser: true,
   },
 }
-const app = e()
 
-// app.use(bodyParser.json())
+const app = e()
+app.use(bodyParser.json())
+
 // statics
 app.all(
   '/api/usuario',
@@ -20,13 +22,4 @@ app.all(
 
 app.use(ApiError.handler)
 
-
-
 export default app
-
-// > POST /api/usuario HTTP/1.1
-// > Host: localhost:3000
-// > Content-Type: application/json
-// > User-Agent: insomnia/8.4.5
-// > Accept: */*
-// > Content-Length: 69
