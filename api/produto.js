2b20d33ac029e56e0aa8d5981037ef669657ee63
marcelotvieira <marcelodefy15@gmail.com>
2024-03-06 12:58:15 -0300

    Feat - Added validation middleware

--- api/produto.js
+++ api/produto.js
@@ -1,14 +1,15 @@
-import bodyParser from 'body-parser'
 import e from 'express'
 import rescue from 'express-rescue'
 import { ProdutoController } from '../modules/controllers/produto'
 import { ApiError } from '../modules/error/ApiError'
+import { validateProductSchemas } from '../modules/middlewares/validateAction'
 
 const app = e()
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 app.all(
   '/api/produto',
+  validateProductSchemas,
   // validateActionMiddleware,
   rescue((req, res) =>
     ProdutoController[req.method.toLowerCase()](req, res)
