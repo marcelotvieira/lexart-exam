4653c004f7a4157e532e4830bdb161a1802f8348
marcelotvieira <marcelodefy15@gmail.com>
2024-03-07 10:54:52 -0300

    Feat - Added Manage Buttons (update, delete) to product Item

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,4 +1,5 @@
-import { Badge, Collapse, Descriptions, Table } from "antd";
+import { DeleteFilled, EditFilled } from "@ant-design/icons";
+import { Badge, Button, Collapse, Descriptions, Table } from "antd";
 
 export function ProductItem({ product }) {
   return (
@@ -6,6 +7,12 @@ export function ProductItem({ product }) {
       <Descriptions.Item label="Nome">{product.name}</Descriptions.Item>
       <Descriptions.Item label="Marca">{product.brand}</Descriptions.Item>
       <Descriptions.Item label="Modelo">{product.model}</Descriptions.Item>
+      <Descriptions.Item label="Gerenciar">
+        <div className="flex aligned-center gap1">
+          <Button href={`/products/update/${product.id}`} icon={<EditFilled />} />
+          <Button icon={<DeleteFilled />} />
+        </div>
+      </Descriptions.Item>
 
       <div>
         <Descriptions.Item >
