251805f329ab1bbcdd5e99be4b06bcb6fb4b8a6c
marcelotvieira <marcelodefy15@gmail.com>
2024-03-07 13:39:38 -0300

    Feat - Turn authorization needed on app routes.

--- api/produto/[id].js
+++ api/produto/[id].js
@@ -3,6 +3,7 @@ import e from 'express'
 import rescue from 'express-rescue'
 import { ProdutoController } from '../../modules/controllers/produto'
 import { ApiError } from '../../modules/error/ApiError'
+import { validateJwt } from '../../modules/middlewares/validateJwt'
 import { validateProductUpdateSchemas } from '../../modules/middlewares/validateSchemas'
 
 const app = e()
@@ -10,19 +11,22 @@ app.use(bodyParser.json())
 
 app.put(
   '/api/produto/:id',
-  validateProductUpdateSchemas,
+  rescue(validateProductUpdateSchemas),
+  rescue(validateJwt),
   rescue((req, res) =>
     ProdutoController.put(req, res)
   ))
 
 app.get(
   '/api/produto/:id',
+  rescue(validateJwt),
   rescue((req, res) =>
     ProdutoController.getOne(req, res)
   ))
 
 app.delete(
   '/api/produto/:id',
+  rescue(validateJwt),
   rescue((req, res) =>
     ProdutoController.delete(req, res)
   ))
